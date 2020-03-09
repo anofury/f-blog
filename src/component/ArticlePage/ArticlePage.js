c818e3570b8631f98a82269b9876e5bb31de592a
anofury <anofury@gmail.com>
2020-03-10 03:44:30 +0800

    page done

--- src/component/ArticlePage/ArticlePage.js
+++ src/component/ArticlePage/ArticlePage.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { observer } from 'mobx-react'
 import BScroll from 'better-scroll'
 import ArticleBlock from '../ArticleBlock/ArticleBlock'
-import { cns } from '../../utils'
+import { cns, sleep } from '../../utils'
 
 import './ArticlePage.css'
 
@@ -13,6 +13,7 @@ class ArticlePage extends BaseComponent {
         super(props)
         this.initData({
             show: false,
+            close: false,
             showTitle: false,
         })
         this.articleInfo = {}
@@ -20,10 +21,10 @@ class ArticlePage extends BaseComponent {
 
     show = articleInfo => {
         this.articleInfo = articleInfo
-        this.setData({ show: true }, () => {
+        this.setData({ show: true, close: false, showTitle: false }, () => {
             this.articlePageBS = new BScroll(this.refs.articlePage, {
                 click: true,
-                swipeBounceTime: 200,
+                // swipeBounceTime: 200,
                 eventPassthrough: 'horizontal',
                 bounceTime: 400,
                 probeType: 3
@@ -34,19 +35,41 @@ class ArticlePage extends BaseComponent {
                 } else this.setData({ showTitle: false })
             })
         })
+        let pushStateState = {
+            title: '文章详细', url: `#/article_title=${this.articleInfo.title}`
+        }
+        window.history.pushState(pushStateState, pushStateState.title, pushStateState.url)
+        window.addEventListener("popstate", this.close, false)
+    }
+
+    close = () => {
+        window.removeEventListener("popstate", this.close, false)
+        this.setData({ close: true }, () => {
+            sleep(400).then(() => {
+                this.setData({ show: false, close: false, showTitle: false })
+                this.articlePageBS.destroy()
+            })
+        })
+    }
+
+    onTapCloseArticlePage = () => {
+        window.history.back()
     }
 
     render() {
-        const { show, showTitle } = this.data
+        const { show, close, showTitle } = this.data
         return (
             show &&
-            <div className='article-page' >
-                <div className='page-header' ref='articleHead'>
-                    <span className='icon-back'></span>
-                    <span className={cns('header-note', { showTitle })}>{showTitle ? this.articleInfo.title : '文章详细'}</span>
+            <div className={cns('article-page', { close })} >
+                <div className='article-page-header' ref='articleHead'>
+                    <p>
+                        <span className='article-header-back icon-back' onClick={this.onTapCloseArticlePage}></span>
+                        <span className='article-header-note'>文章详细</span>
+                    </p>
+                    <span className={cns('article-header-title', { show: showTitle })}>{this.articleInfo.title}</span>
                 </div>
-                <div className='page-content' ref='articlePage'>
-                    <div className='page-content-BS'>
+                <div className='article-page-content' ref='articlePage'>
+                    <div className='article-page-content-BS'>
                         <ArticleBlock article={this.articleInfo} showAll={true} />
                     </div>
                 </div>
