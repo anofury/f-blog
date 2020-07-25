75f2e5c3efb4e8c517649d81315ae4f70d4f55d3
anofury <anofury@gmail.com>
2020-07-25 19:22:52 +0800

    youhua

--- src/box/box.js
+++ src/box/box.js
@@ -10,14 +10,11 @@ import './box.css'
 export default class Box extends BaseComponent {
     constructor(props) {
         super(props)
-        this.initData({
-            loopCountDown: 0
-        })
         this.timer = 0
         this.imgList = []
         this.config = {
             fadeInTime: 5,
-            loopTime: 10,
+            loopTime: 8,
             buttons: [
                 {
                     title: '詞霸',
@@ -41,7 +38,6 @@ export default class Box extends BaseComponent {
 
     clearTimer = () => {
         clearTimeout(this.timer)
-        clearInterval(this.countDownTimer)
     }
 
     swiperChangeHandle(current) {
@@ -54,18 +50,9 @@ export default class Box extends BaseComponent {
         }
     }
 
-    countDown = () => {
-        this.setData({ loopCountDown: this.config.loopTime + this.config.fadeInTime })
-        this.countDownTimer = setInterval(() => {
-            const loopCountDown = this.getData.loopCountDown
-            this.setData({ loopCountDown: loopCountDown - 1 })
-            this.getData.loopCountDown <= 0 && clearInterval(this.countDownTimer)
-        }, 1000)
-    }
-
     getRandomImg = () => {
         let newSrc = BGs.dir + BGs.list[~~(Math.random() * BGs.length)]
-        if (newSrc === this.getData.currentSrc)
+        if (this.imgList.length && this.imgList[0].src.indexOf(newSrc) !== -1)
             return this.getRandomImg()
         else
             return newSrc
@@ -74,8 +61,6 @@ export default class Box extends BaseComponent {
     loadRandomImg = (init = false) => {
         const { fadeInTime, loopTime } = this.config
         this.clearTimer()
-        this.countDown()
-        !init && this.showOath()
         this.timer = setTimeout(() => {
             preLoadImg(this.getRandomImg()).then(Img => {
                 let boxImg = Img[0]
@@ -83,26 +68,23 @@ export default class Box extends BaseComponent {
 
                 this.imgList.push(boxImg)
                 this.refs.box.appendChild(boxImg)
+                !init && this.showOath()
 
                 setTimeout(() => {
                     if (this.imgList.length >= 2) {
                         this.refs.box.removeChild(this.imgList[0])
                         this.imgList.shift()
+                        this.loadRandomImg()
                     }
-                    this.loadRandomImg()
                 }, fadeInTime * 1000)
             })
-        }, init ? 0 : loopTime * 1000)
+        }, init ? 0 : (loopTime * 1000))
     }
 
     showOath = () => {
         OKFetch(fetchParam.oath).then(oath => {
-            Toast.show({ text: oath.data.content })
+            Toast.show({ text: oath.data.content, last: 5000 })
         })
-    }
-
-    onTapBoxBtn = () => {
-
     }
 
     componentWillMount() {
@@ -114,21 +96,12 @@ export default class Box extends BaseComponent {
                 this.loadRandomImg(!this.imgList.length)
             }
         })
-        OKFetch(fetchParam.yiyan).then(data => {
-            console.log(data)
-        })
     }
 
     render() {
-        const { loopCountDown } = this.data
         return (
             <div className='blog-box' ref='box'>
-                <span className='box-countdown'>{loopCountDown}</span>
-                <div className='box-container'>{
-                    this.config.buttons.map((btn, index) =>
-                        <div key={index} onClick={this.onTapBoxBtn.bind(this, btn)}>{btn.title}</div>
-                    )
-                }</div>
+                <div className='box-container'></div>
             </div>
         )
     }
